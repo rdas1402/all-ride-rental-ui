51aa7f6bd502bb2822646203c7d2c95871a2dce9
rdas1402 <r14.das@gmail.com>
2021-07-03 14:57:33 +0530

    deploy first instance of All ride rental

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import profileImage from './img/AllRideRental.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>All Ride Rental</h1>
+      <h2>Author: Rupam Das</h2>
+      <img src={profileImage} alt="displayPicture"/>
+    </div>
+  );
+}
+
+export default App;
